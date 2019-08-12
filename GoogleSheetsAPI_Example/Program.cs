21d6d5747072c77b2a0309e14727950eefca05db
IT-school.com.ua <37939704+it-school@users.noreply.github.com>
2019-08-12 21:51:55 +0300

    Update Program.cs

--- GoogleSheetsAPI_Example/Program.cs
+++ GoogleSheetsAPI_Example/Program.cs
@@ -12,54 +12,6 @@ namespace GoogleSheetsAPI_Example
 {
     class Program
     {
-        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
-        static string ApplicationName = "Google Sheets API .NET Example";
-
-        static SheetsService GetService()
-        {
-            UserCredential credential;
-
-            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
-            {
-                // Файл token.json хранит маркеры доступа и обновления для пользователя и создается автоматически при первом завершении потока авторизации.
-                string credPath = "token.json";
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
-                Console.WriteLine("Credential file saved to: " + credPath);
-            }
-
-            // Создаём сервис Google Sheets API
-            var service = new SheetsService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = ApplicationName,
-            });
-
-            return service;
-        }
-
-        static void GetDataFromGoogleSheetByID(string spreadsheetId, string range)
-        {
-            SheetsService service = GetService();
-            // Определим параметры запроса
-            SpreadsheetsResource.ValuesResource.GetRequest request = service.Spreadsheets.Values.Get(spreadsheetId, range);
-
-            // Печатаем ID, имя и счёт пользователя из таблицы
-            ValueRange response = request.Execute();
-            IList<IList<Object>> values = response.Values;
-            if (values != null && values.Count > 0)
-            {
-                Console.WriteLine("ID\tName\tAccount");
-                foreach (var row in values)
-                    Console.WriteLine($"{String.Format("{0,-10}", row[0])}\t{String.Format("{0,-8}", row[1])}\t{String.Format("{0,-8}", row[4])}");
-            }
-            else
-                Console.WriteLine("Нет данных");
-        }
 
         static void CreateNewSheet()
         {
@@ -109,10 +61,26 @@ namespace GoogleSheetsAPI_Example
 
         static void Main(string[] args)
         {
-            GetDataFromGoogleSheetByID("1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms", "Class Data!A2:E");
-            //GetDataFromGoogleSheetByID("1d8RetomIySKDZp1e0bGSuDlKUEfiQQQR7uapifHABT0", "Class Data!A2:C");
+           String spreadsheetId = "1DsTn1vCGNm38Kjcts6E6yBwd_IhmmMejjBXqkJIiCCU";
+            //spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+            String range = "Sheets1!A2:C";
+            GSheets sheets = new GSheets();
+            IList<IList<Object>>  result = sheets.GetData(spreadsheetId, range);
+            if (result != null && result.Count > 0)
+            {
+                Console.WriteLine("ID, Name, Major");
+                foreach (var row in result)
+                {
+                    Console.WriteLine("{0}, {1}, {2}", row[0], row[1], row[2]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
 
-            CreateNewSheet();
+            Console.WriteLine("DataAdd:\n");
+            sheets.AddData(spreadsheetId, range);
         }
     }
 }
